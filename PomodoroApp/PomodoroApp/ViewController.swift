1f2c13fcf470f974581747eafa07570bc8bc5c71
Georgiy <33875528+georg1856@users.noreply.github.com>
2022-05-22 21:27:00 +1000

    developing methods for buttons

--- PomodoroApp/PomodoroApp/ViewController.swift
+++ PomodoroApp/PomodoroApp/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController, CAAnimationDelegate  {
     
@@ -38,6 +39,19 @@ class ViewController: UIViewController, CAAnimationDelegate  {
         return cancelButton
     }()
     
+    var player: AVAudioPlayer! = nil
+    var timer = Timer()
+    var isTimerStarted = false
+    var time = 10
+    var isAnimationStarted = false
+    var isWorkTime = true
+    
+    let progressLayerWork = CAShapeLayer()
+    let progressLayerRelax = CAShapeLayer()
+    let backProgressLayer = CAShapeLayer()
+    let animation = CABasicAnimation(keyPath: "strokeEnd")
+    let animationRelax = CABasicAnimation(keyPath: "strokeEnd")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -51,6 +65,107 @@ class ViewController: UIViewController, CAAnimationDelegate  {
                                action: #selector(cancelButtonTapped),
                                for: .touchUpInside)
     }
+    
+    @objc func startButtonTapped() {
+        if isWorkTime {
+            cancelButton.isEnabled = true
+            cancelButton.alpha = 1
+            if !isTimerStarted {
+                startResumeAnimation()
+                startTimer()
+                isTimerStarted = true
+                startButton.setImage(UIImage(systemName: "pause"), for: .normal)
+                startButton.tintColor = UIColor.black
+            } else {
+                pauseAnimation(with: progressLayerWork)
+                timer.invalidate()
+                isTimerStarted = false
+                startButton.setImage(UIImage(systemName: "play"), for: .normal)
+                startButton.tintColor = UIColor.red
+            }
+        } else {
+            cancelButton.isEnabled = true
+            cancelButton.alpha = 1
+            if !isTimerStarted {
+                startResumeAnimation()
+                startTimer()
+                isTimerStarted = true
+                startButton.setImage(UIImage(systemName: "pause"), for: .normal)
+                startButton.tintColor = UIColor.black
+            } else {
+                pauseAnimation(with: progressLayerRelax)
+                timer.invalidate()
+                isTimerStarted = false
+                startButton.setImage(UIImage(systemName: "play"), for: .normal)
+                startButton.tintColor = UIColor.green
+            }
+        }
+    }
+    
+    @objc func cancelButtonTapped() {
+        if isWorkTime {
+            stopAnimation(with: progressLayerWork)
+            cancelButton.isEnabled = false
+            cancelButton.alpha = 0.5
+            
+            timer.invalidate()
+            time = 5
+            isTimerStarted = false
+            timerLabel.text = "00:05"
+            startButton.setImage(UIImage(systemName: "play"), for: .normal)
+            startButton.tintColor = UIColor.red
+            drawBackLayer(with: progressLayerWork, color: UIColor.red)
+            
+        } else {
+            stopAnimation(with: progressLayerRelax)
+            cancelButton.isEnabled = false
+            cancelButton.alpha = 0.5
+            
+            timer.invalidate()
+            time = 5
+            isTimerStarted = false
+            timerLabel.text = "00:05"
+            startButton.setImage(UIImage(systemName: "play"), for: .normal)
+            startButton.tintColor = UIColor.green
+            drawBackLayer(with: progressLayerRelax, color: UIColor.green)
+        }
+    }
+    
+    @objc func updateTimer() {
+        if time < 1 && !isWorkTime {
+            startButton.setImage(UIImage(systemName: "play"), for: .normal)
+            startButton.tintColor = UIColor.red
+            cancelButton.tintColor = UIColor.red
+            time = 10
+            timerLabel.text = "00:10"
+            timerLabel.textColor = UIColor.red
+            timer.invalidate()
+            isTimerStarted = false
+            isWorkTime = true
+            drawBackLayer(with: backProgressLayer, color: UIColor.gray)
+            drawBackLayer(with: progressLayerWork, color: UIColor.red)
+            isAnimationStarted = false
+            music()
+        } else if time < 1 {
+            startButton.setImage(UIImage(systemName: "play"), for: .normal)
+            startButton.tintColor = UIColor.green
+            time = 5
+            timerLabel.text = "00:05"
+            timerLabel.textColor = UIColor.green
+            timer.invalidate()
+            isTimerStarted = false
+            isWorkTime = false
+            drawBackLayer(with: backProgressLayer, color: UIColor.gray)
+            drawBackLayer(with: progressLayerRelax, color: UIColor.green)
+            cancelButton.tintColor = UIColor.green
+            isAnimationStarted = false
+            music()
+        } else {
+            time -= 1
+            timerLabel.text = formatTime()
+        }
+    }
+    
     
     func setConstraints() {
         view.addSubview(startButton)
@@ -77,8 +192,17 @@ class ViewController: UIViewController, CAAnimationDelegate  {
         ])
     }
     
+    func startTimer() {
+        timer  = Timer.scheduledTimer(timeInterval: 1,
+                                      target: self,
+                                      selector: (#selector(updateTimer)),
+                                      userInfo: nil,
+                                      repeats: true)
+    }
     
-
-
+    func formatTime() -> String {
+        let minutes = Int(time) / 60 % 60
+        let seconds = Int(time) % 60
+        return String(format: "%02i:%02i", minutes, seconds)
+    }
 }
-
